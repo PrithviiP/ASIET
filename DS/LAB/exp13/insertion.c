5efcc6be3bb08b49e622bbb7b1a24629dde41798
PrithviiP <prithvipramod00@gmail.com>
2024-11-05 10:09:43 +0530

    Add files via upload

---
+++ DS/LAB/exp13/insertion.c
@@ -0,0 +1,45 @@
+//prithvi p
+//rollno 48
+
+#include <stdio.h>
+
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n) 
+{
+    for (int i = 1; i < n; ++i) 
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n,i,arr[50];
+    printf("Enter the no of elements in array:");
+    scanf("%d", &n);
+    
+    printf("Enter the elements:");
+    for(i = 0; i < n; i++)
+    {
+    	scanf("%d", &arr[i]);
+    }    
+    
+    insertionSort(arr,n);
+    printf("Sorted array is:");
+    printArray(arr,n);
+
+    return 0;
+}
+
